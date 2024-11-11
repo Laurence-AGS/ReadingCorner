62f06bb7f2aa47feb9696f36a15c87cab93198a4
ronyramez <rony.ramez@hotmail.com>
2024-11-11 21:16:23 +0200

    Edits to write in db

--- ReadingCorner/ReadingCorner/settings.py
+++ ReadingCorner/ReadingCorner/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'testapp',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -75,10 +77,15 @@ WSGI_APPLICATION = 'ReadingCorner.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'readingcorner',
+        'USER': 'rony',
+        'PASSWORD': 'rony1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation

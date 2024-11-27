8b20eb21d405fc949daac89c391575cd5c529d2b
Laurece-AGS <s-laurence.shehata@zewailcity.edu.eg>
2024-11-27 22:17:28 +0200

    .

--- FrontEnd/app/(tabs)/index.tsx
+++ FrontEnd/app/(tabs)/index.tsx
@@ -6,7 +6,7 @@ import { StatusBar } from 'expo-status-bar';
 export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 items-center justify-center bg-[#EDDFE0]">
-      <Link href="/profile" className='text-blue-800'>
+      <Link href="/profile">
       <StatusBar/>
       <Text className='text-3xl text-[#705C53]'>Go to profile</Text>
       </Link>

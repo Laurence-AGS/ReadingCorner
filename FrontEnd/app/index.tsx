529677d2e9d0bf2ff2d0a7804b1c897c58f178d9
Laurece-AGS <s-laurence.shehata@zewailcity.edu.eg>
2024-12-19 22:51:21 +0200

    editing mock data file

--- FrontEnd/app/index.tsx
+++ FrontEnd/app/index.tsx
@@ -7,47 +7,53 @@ import { supabase } from "@/lib/supabase";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function HomeScreen() {
-    const [session, setSession] = useState<Session | null>(null);
+  const [session, setSession] = useState<Session | null>(null);
 
-    useEffect(() => {
-        // Fetch the initial session
-        supabase.auth.getSession().then(({ data: { session } }) => {
-            setSession(session);
-        });
+  useEffect(() => {
+    // Fetch the initial session
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
 
-        // Listen for auth state changes
-        const { data: authListener } = supabase.auth.onAuthStateChange((_event, session) => {
-            setSession(session);
-        });
-
-        // Handle app state changes for session auto-refresh
-        const handleAppStateChange = (state: AppStateStatus) => {
-            if (state === "active") {
-                supabase.auth.startAutoRefresh();
-            } else {
-                supabase.auth.stopAutoRefresh();
-            }
-        };
-
-        // Subscribe to app state changes
-        const subscription = AppState.addEventListener("change", handleAppStateChange);
-
-        // Clean up
-        return () => {
-            subscription.remove();
-            authListener?.subscription.unsubscribe();
-        };
-    }, []);
-
-    return (
-        <GestureHandlerRootView style={styles.container}>
-            {session && session.user ? <Redirect href="/(root)" /> : <Redirect href="/(auth)" />}
-        </GestureHandlerRootView>
+    // Listen for auth state changes
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setSession(session);
+      }
     );
+
+    // Handle app state changes for session auto-refresh
+    const handleAppStateChange = (state: AppStateStatus) => {
+      if (state === "active") {
+        supabase.auth.startAutoRefresh();
+      } else {
+        supabase.auth.stopAutoRefresh();
+      }
+    };
+
+    // Subscribe to app state changes
+    const subscription = AppState.addEventListener(
+      "change",
+      handleAppStateChange
+    );
+
+    // Clean up
+    return () => {
+      subscription.remove();
+      authListener?.subscription.unsubscribe();
+    };
+  }, []);
+
+  return (
+    <GestureHandlerRootView style={styles.container}>
+      {/* {session && session.user ? <Redirect href="/(root)" /> : <Redirect href="/(auth)" />} */}
+      <Redirect href="/(root)" />
+    </GestureHandlerRootView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
+  container: {
+    flex: 1,
+  },
 });
